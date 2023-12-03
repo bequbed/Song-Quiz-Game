d9c7efb48f071f9b9fad6542e31d420ca2663860
bequbed <107822319+bequbed@users.noreply.github.com>
2023-12-04 12:05:59 +1300

    Update song_quiz.py

--- song_quiz.py
+++ song_quiz.py
@@ -1,33 +1,58 @@
 import random
+import time
 
 class SongQuiz:
-    def __init__(self, songs):
+    def __init__(self, songs, difficulty_levels):
         self.songs = songs
+        self.difficulty_levels = difficulty_levels
         self.score = 0
+        self.high_score = 0
 
     def start_quiz(self):
-        print("Welcome to the Song Quiz!!")
+        print("Welcome to the Song Quiz!")
 
-        while self.songs:
+        while True:
+            # Select a random song and difficulty level
             current_song = random.choice(self.songs)
-            self.play_song(current_song)
-            user_guess = input("Guess the song title: ").strip().lower()
+            current_difficulty = self.difficulty_levels[current_song]
 
-            if user_guess == current_song.lower():
-                print("Correct!\n")
-                self.score += 1
+            # Display the current score and high score
+            print(f"\nScore: {self.score} | High Score: {self.high_score}")
+
+            # Display the current song and difficulty level
+            print(f"\nNow playing: {current_song} (Difficulty: {current_difficulty})")
+
+            # Start the timer for each question
+            start_time = time.time()
+            user_guess = input("Guess the song title: ").strip().lower()
+            end_time = time.time()
+            time_taken = round(end_time - start_time, 2)
+
+            # Check the user's guess and update the score
+            if user_guess == current_song.lower() and time_taken <= current_difficulty:
+                print(f"Correct! You guessed it in {time_taken} seconds.\n")
+                self.score += current_difficulty
             else:
                 print(f"Wrong! The correct answer is {current_song}\n")
+                self.score = 0  # Reset the score for a wrong answer
 
-            self.songs.remove(current_song)
+            # Update the high score if needed
+            if self.score > self.high_score:
+                self.high_score = self.score
 
-        print(f"Quiz completed! Your final score is {self.score}/{len(songs)}.")
+            # Ask if the user wants to play again
+            play_again = input("Do you want to play again? (yes/no): ").lower()
+            if play_again != 'yes':
+                print("Thanks for playing! Your final score is:", self.high_score)
+                break
 
-    def play_song(self, song):
-        print(f"Now playing: {song}")
+    # ... (Other methods remain the same)
 
 # Example usage:
 if __name__ == "__main__":
-    song_list = ["Song A", "Song B", "Song C", "Song D", "Song E"]
-    quiz = SongQuiz(song_list)
+    song_difficulty = {"Song A": 3, "Song B": 2, "Song C": 1, "Song D": 3, "Song E": 2}
+    song_list = list(song_difficulty.keys())
+
+    quiz = SongQuiz(song_list, song_difficulty)
     quiz.start_quiz()
+
